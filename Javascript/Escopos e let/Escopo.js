59bab7b7471a1e3e2d979761dfa262d31e1f36cf
lukasptta <lukasptta@gmail.com>
2022-06-26 22:15:07 -0300

    update

---
+++ Javascript/Escopos e let/Escopo.js
@@ -0,0 +1,13 @@
+function imprimirNome(){
+  var nome = "LUcas";
+  console.log(nome);
+}
+
+//imprimirNome();
+
+let sobrenome = "Maciel";
+console.log(sobrenome);
+{
+ let sobrenome = "pita" 
+ console.log(sobrenome);
+}
---
+++ Javascript/Escopos e let/desafio.js
@@ -0,0 +1,29 @@
+/*let N=parseInt(gets())
+let pares=[]
+let impares=[]
+for(leti=0;i<N;i++){
+  let num=parseInt(gets())
+ num%2===0?pares.push(num):impares.push(num)
+}
+pares.sort((a,b)=>a-b)
+impares.sort((a,b)=>b-a)
+for(e of[pares,impares]){
+ console.log(e)
+}
+
+function printPatos(p){
+  for(var i=0; i <p.length; i++){
+
+      if(p[i]== 0){
+        console.log(0)
+      }else if(p[i]== -1){
+        console.log ()
+      }else if(p[i]!= 0){
+        console.log(p[i]-1)
+      }
+    }
+
+  }
+
+var p = [0,2,5,8,-1,1,6,-1];
+printPatos(p);*/
